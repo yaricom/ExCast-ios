8c3f1aa178c34836bf1e757c29fd02c603a15806
yaric_om <yaric_mail@yahoo.com>
2016-02-29 16:33:12 +0200

    Added core data controller, fixed copyright text in added classes

---
+++ Classes/CVCoreDataController.m
@@ -0,0 +1,13 @@
+//
+//  CVCoreDataController.m
+//  CastVideos
+//
+//  Created by Iaroslav Omelianenko on 2/29/16.
+//  Copyright © 2016 Google inc. All rights reserved.
+//
+
+#import "CVCoreDataController.h"
+
+@implementation CVCoreDataController
+
+@end
